ce13c62a773c101de2df877b76baaa80b74e54df
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-09-16 07:30:32 +0800

    .

--- src/main/java/me/kall/blocktracker/api/Trackable.java
+++ src/main/java/me/kall/blocktracker/api/Trackable.java
@@ -1,20 +1,6 @@
 package me.kall.blocktracker.api;
 
-import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
-import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
-import it.unimi.dsi.fastutil.longs.LongSet;
-import it.unimi.dsi.fastutil.longs.LongSets;
-import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectList;
-import me.kall.blocktracker.data.BlockTracker;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.ForgeRegistries;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public interface Trackable {
     boolean trackable$isTracked();
@@ -29,68 +15,5 @@ public interface Trackable {
 
     static boolean acceptWorldGen(Block block) {
         return ((Trackable)block).worldGen$accepted();
-    }
-
-    static void trackBlock(Block block) {
-        BlockTracker.TRACKED_BLOCKS.put(ForgeRegistries.BLOCKS.getKey(block), true);
-    }
-
-    static void trackBlock(ResourceLocation block) {
-        BlockTracker.TRACKED_BLOCKS.put(block, true);
-    }
-
-    static void trackBlock(Block block, boolean acceptWorldGen) {
-        BlockTracker.TRACKED_BLOCKS.put(ForgeRegistries.BLOCKS.getKey(block), acceptWorldGen);
-    }
-
-    static void trackBlock(ResourceLocation block, boolean acceptWorldGen) {
-        BlockTracker.TRACKED_BLOCKS.put(block, acceptWorldGen);
-    }
-
-    static LongSet getTrackedBlocks(ServerLevel level, ChunkPos chunkPos, Block block) {
-        return getTrackedBlocks(level, chunkPos, ForgeRegistries.BLOCKS.getKey(block));
-    }
-
-    static LongSet getTrackedBlocks(ServerLevel level, @NotNull ChunkPos chunkPos, @Nullable ResourceLocation blockId) {
-        return getTrackedBlocks(level, chunkPos.toLong(), blockId);
-    }
-
-    static LongSet getTrackedBlocks(ServerLevel level, long chunkPos, @Nullable ResourceLocation blockId) {
-        if (blockId == null) return LongSets.emptySet();
-        if (!BlockTracker.TRACKED_BLOCKS.containsKey(blockId)) return LongSets.emptySet();
-        Long2ObjectMap<Object2ObjectMap<ResourceLocation, LongSet>> chunkMap = BlockTracker.get(level).blockStorage.get(level.dimension().location());
-        if (chunkMap == null) return LongSets.emptySet();
-        Object2ObjectMap<ResourceLocation, LongSet> blockMap = chunkMap.get(chunkPos);
-        if (blockMap == null) return LongSets.emptySet();
-        LongSet blocks = blockMap.get(blockId);
-        return blocks == null ? LongSets.emptySet() : LongSets.unmodifiable(blocks);
-    }
-
-    static @NotNull ObjectList<LongSet> getTrackedBlocks(ServerLevel level, @NotNull ChunkPos center, int aroundRadius, @Nullable ResourceLocation blockId) {
-        ObjectList<LongSet> tracked = new ObjectArrayList<>();
-
-        int minX = center.x - aroundRadius;
-        int maxX = center.x + aroundRadius;
-        int minZ = center.z - aroundRadius;
-        int maxZ = center.z + aroundRadius;
-
-        for (int x = minX; x <= maxX; x++) {
-            for (int z = minZ; z <= maxZ; z++) {
-                LongSet blocks = getTrackedBlocks(level, ChunkPos.asLong(x, z), blockId);
-                if (blocks.isEmpty()) continue;
-                tracked.add(blocks);
-            }
-        }
-
-        return tracked;
-    }
-
-    static @NotNull LongSet getTrackedBlocksFlat(ServerLevel level, @NotNull ChunkPos center, int aroundRadius, @Nullable ResourceLocation blockId) {
-        LongSet tracked = new LongOpenHashSet();
-        for (LongSet blocks : getTrackedBlocks(level, center, aroundRadius, blockId)) {
-            if (blocks.isEmpty()) continue;
-            tracked.addAll(blocks);
-        }
-        return tracked;
     }
 }
